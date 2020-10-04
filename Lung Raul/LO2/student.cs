ac62696c4c145bf894075e224b0cef210a41e8a0
LungRaul <62697969+LungRaul@users.noreply.github.com>
2020-10-04 22:59:30 +0300

    Add files via upload

---
+++ Lung Raul/LO2/student.cs
@@ -0,0 +1,10 @@
+namespace stud{
+ public class Studenti
+ {
+  public int Id{get;set;}
+  public string nume {get;set;}
+  public string prenume{get;set;}
+  public string fac{get;set;}
+  public int anStd{get;set}
+ }
+}
