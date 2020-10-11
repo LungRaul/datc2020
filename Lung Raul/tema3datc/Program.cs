2c6312aef5970cf919a4aa22fa4fdb76ef3f4e51
LungRaul <62697969+LungRaul@users.noreply.github.com>
2020-10-11 13:09:16 +0300

    Add files via upload

---
+++ Lung Raul/tema3datc/Program.cs
@@ -0,0 +1,52 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Drive.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace tema3datc
+{
+    class Program
+    {
+         static string[] Scopes = { DriveService.Scope.Drive };
+        static string ApplicationName = "DATC TEMA";
+
+        static void Main(string[] args)
+        {
+            Initializare();
+        }
+        static void Initializare()
+        {
+             UserCredential credential;
+
+            using (var stream =
+                new FileStream("client_secret_376976299182-q4go9tvo6ukf407egtuonc38uqukk8m4.apps.googleusercontent.com.json", FileMode.Open, FileAccess.Read))
+            {
+                // The file token.json stores the user's access and refresh tokens, and is created
+                // automatically when the authorization flow completes for the first time.
+                string credPath = "token";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    Environment.UserName,
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            // Create Drive API service.
+            var service = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+        }
+    }
+}
